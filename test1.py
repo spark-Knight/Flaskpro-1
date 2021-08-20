95f346101de9f02973ab48114bfac7b09fd2d0a2
spark-Knight <67824440+spark-Knight@users.noreply.github.com>
2021-08-20 16:25:48 +0530

    this is my juggad dict

---
+++ test1.py
@@ -0,0 +1,35 @@
+juggad = {"what is juggad trust" : "juggad is unit where we make product with waste materials.",
+        "there are two units" : ["Paper unit","Stitching unit"],
+        "honour" : "Veena di",
+        "Stitching unit" : {
+            "Timing" : "10:00 AM to 17:00 PM",
+            "Stitching unit Members" : ["Shivkaali","Bala","Sadhana","babita","kusum","Sangeeta"],
+            "Stitching Machine" : 15,
+            "Manager" : "Shabila di",
+            "Training Children" : ["Shama" , "Geeta" ,"Reahna" ,"Pooja" ,"Jyoti"],
+            "production" : ["Rakhi","Bag","Mask","pencil pouch","Potli","Purse","Necklace","Earrings","Rubber","Braslet","Keychain"],
+            "Types of Rakhies" : ["RJ=1","RJ=1","RJ=2","RJ=3","RJ=4","RJ=5","RJ=6","RJ=7","RJ=8","RJ=9","RJ=10","RJ=11","RJ=12"],
+            "Types of Bags" : ["Toilet Bag" ,"Animal Bag" ,"Owl Bag" ,"Tote Bag" ,"School Bag" ,"Bottle Bag" ,"Cameras Bag" ,"Wire Wrapper" ,"Weekan Bag" , "Round Compact Shoping Bag"],
+            "Types of Earrings" : ["Luppi Earrings" ,"One Borria Earrings" ,"Four Borria Earrings" ,"Embridrey two Borria Earrings"],
+            "Types of Necklace" : ["Dream Catcher Necklace"],
+            "Types of Keychains" : ["Fish Keychain" ,"Catterpillar Keychain" ,"Foot Keychain" ,"Leaf Keychain" ,"Ring Keychain Small" ,"Ring Keychain Large" ],
+        },
+        "Paper unit" : {
+            "Timing" : "10:00 AM to 17 PM",
+            "Paper Unit Members" : ["Rakesh bhaiya" ,"Pardeep bhaiya"],
+            "Tools" : ["Hammer" , "Tape Measure" ,"Pencil" ,"Nail Puller" ,"Levels" ,"Drill"],
+            "Manager" : "Rakesh bhaiya" ,
+            "Training Children" : ["Ashish" ,"Sachin" ,"Sohan" ,"Simran" ,"Umeresh" ,"devraj" ,"Raju" ," tushar" ,"Vishal (s)" ,"Vishal (b)"],
+            "Classes of this unit" : ["Phone repairing" ,"Carpentry" ],
+            "production" : ["Diye" ,"Candles" ,"Bag" ,"Dairies" ,"Bookmark" ,"Envelope" ,"Planter" ],
+            "Types of Diye" : ["Cocunut diye" ,"Lemon diye" ,"Malta diye" ,"Cocunut Shell diye" ,"Matka Diye" ,"Glass diye"],
+            "Types of Candles" : ["Cocunut Candles" ,"Wood Candles" ,"WAter Floating Candles" ,"Cocunut Shell Candles" ,"Bamboo Candles"],
+            "Types of Bags" : ["Paper Bags" ,"Kite Paper Bags" ,"Newspaper Bags" ,"Polly paper Bags"],
+            "Types of Dairies" : ["Elastic Dairies" ,"Button Dairies" ,"Rubber Dairies"],
+            "Types of Planters" : ["bottle Planters" ,"Cocunut Planters" ,"Cocunut shell Planters"],
+        }
+
+    }
+
+
+print(juggad)    
